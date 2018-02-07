c59c174f7f30e957baa829ac97b8fed0d1aed4ef
JacekDwojakPL <dwojak.jacek@gmail.com>
2018-02-08 00:13:14 +0100

    changed asset image

--- application.py
+++ application.py
@@ -6,3 +6,15 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/instrument/<name>")
+def product(name):
+    products = [{'name': 'majestic 1', 'opis': 'dupa1'},
+                {'name': 'majestic 2', 'opis': 'dupa2'},
+                {'name': 'majestic 3', 'opis': 'dupa3'},
+                {'name': 'majestic 4', 'opis': 'dupa4'},
+                {'name': 'majestic 5', 'opis': 'dupa5'},
+                {'name': 'majestic 6', 'opis': 'dupa6'},
+                {'name': 'majestic 7', 'opis': 'dupa7'},]
+
+    return render_template("product.html", products=products)
