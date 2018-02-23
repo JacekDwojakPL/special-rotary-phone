e90f7e080d1ed15f1315a01ba0405dd8bae1ae75
JacekDwojakPL <dwojak.jacek@gmail.com>
2018-02-23 14:36:54 +0100

    poprawiono blad z wyswietlaniem opisow przy instrumentach klawiszowych. dodano testowe opisy instrumentow

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-$('.ajax-trigger').on('click', function () {
+$('.ajax-trigger-percussion').on('click', function () {
   var parameters = {
     instrument_type: $(this).attr('id')
   };
@@ -6,17 +6,41 @@ $('.ajax-trigger').on('click', function () {
   $.getJSON(Flask.url_for("description"), parameters)
     .done(function(data, textStatus, jqXHR){
 
-    $("#instrument_name").fadeOut(50, function() {
+    $("#percussion_instrument_name").fadeOut(50, function() {
 
-      $("#instrument_name").html(data['name']);
-      $("#instrument_name").fadeIn();
+      $("#percussion_instrument_name").html(data['name']);
+      $("#percussion_instrument_name").fadeIn();
 
     });
 
-    $("#instrument_description").fadeOut(50, function () {
+    $("#percussion_instrument_description").fadeOut(50, function () {
 
-      $("#instrument_description").html(data['opis']);
-      $("#instrument_description").fadeIn();
+      $("#percussion_instrument_description").html(data['opis']);
+      $("#percussion_instrument_description").fadeIn();
+    });
+
+  });
+});
+
+$('.ajax-trigger-keyboard').on('click', function () {
+  var parameters = {
+    instrument_type: $(this).attr('id')
+  };
+
+  $.getJSON(Flask.url_for("description"), parameters)
+    .done(function(data, textStatus, jqXHR){
+
+    $("#keyboard_instrument_name").fadeOut(50, function() {
+
+      $("#keyboard_instrument_name").html(data['name']);
+      $("#keyboard_instrument_name").fadeIn();
+
+    });
+
+    $("#keyboard_instrument_description").fadeOut(50, function () {
+
+      $("#keyboard_instrument_description").html(data['opis']);
+      $("#keyboard_instrument_description").fadeIn();
     });
 
   });
